93ae85d810075783bd7b70658945e71b266a104f
tshemsedinov <timur.shemsedinov@gmail.com>
2019-02-13 13:37:54 +0200

    Add deadlock and livelock examples

---
+++ JavaScript/2-deadlock.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const threads = require('worker_threads');
+const { Worker, isMainThread } = threads;
+
+const LOCKED = 0;
+const UNLOCKED = 1;
+
+class Mutex {
+  constructor(shared, offset = 0, initial = false) {
+    this.lock = new Int32Array(shared, offset, 1);
+    if (initial) Atomics.store(this.lock, 0, UNLOCKED);
+    this.owner = false;
+  }
+
+  enter(callback) {
+    Atomics.wait(this.lock, 0, LOCKED);
+    Atomics.store(this.lock, 0, LOCKED);
+    this.owner = true;
+    setTimeout(callback, 0);
+    return true;
+  }
+
+  leave() {
+    if (!this.owner) return false;
+    Atomics.store(this.lock, 0, UNLOCKED);
+    Atomics.notify(this.lock, 0, 1);
+    this.owner = false;
+    return true;
+  }
+}
+
+// Usage
+
+if (isMainThread) {
+  const buffer = new SharedArrayBuffer(8);
+  const mutex1 = new Mutex(buffer, 0, true);
+  const mutex2 = new Mutex(buffer, 4, true);
+  console.dir({ mutex1, mutex2 });
+  new Worker(__filename, { workerData: buffer });
+  new Worker(__filename, { workerData: buffer });
+} else {
+  const { threadId, workerData } = threads;
+  const mutex1 = new Mutex(workerData);
+  const mutex2 = new Mutex(workerData, 4);
+
+  if (threadId === 1) {
+    mutex1.enter(() => {
+      console.log('Entered mutex1 from worker1');
+      setTimeout(() => {
+        mutex2.enter(() => {
+          console.log('Entered mutex2 from worker1');
+          if (mutex1.leave()) console.log('Left mutex1 from worker1');
+          if (mutex2.leave()) console.log('Left mutex2 from worker1');
+        });
+      }, 100);
+    });
+  } else {
+    mutex2.enter(() => {
+      console.log('Entered mutex2 from worker2');
+      // Uncomment to fix deadlock
+      // if (mutex2.leave()) console.log('Left mutex2 from worker2');
+      setTimeout(() => {
+        mutex1.enter(() => {
+          console.log('Entered mutex1 from worker2');
+          if (mutex2.leave()) console.log('Left mutex2 from worker2');
+          if (mutex1.leave()) console.log('Left mutex1 from worker2');
+        });
+      }, 100);
+    });
+  }
+
+}
---
+++ JavaScript/3-livelock.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const threads = require('worker_threads');
+const { Worker, isMainThread } = threads;
+
+const LOCKED = 0;
+const UNLOCKED = 1;
+
+class Mutex {
+  constructor(shared, offset = 0, initial = false) {
+    this.lock = new Int32Array(shared, offset, 1);
+    if (initial) Atomics.store(this.lock, 0, UNLOCKED);
+    this.owner = false;
+  }
+
+  enter(callback) {
+    console.log(`Wait in worker${threads.threadId}`);
+    Atomics.wait(this.lock, 0, LOCKED);
+    console.log(`endWait in worker${threads.threadId}`);
+    Atomics.store(this.lock, 0, LOCKED);
+    this.owner = true;
+    setTimeout(callback, 0);
+    return true;
+  }
+
+  leave() {
+    if (!this.owner) return false;
+    Atomics.store(this.lock, 0, UNLOCKED);
+    Atomics.notify(this.lock, 0, 1);
+    this.owner = false;
+    return true;
+  }
+}
+
+// Usage
+
+if (isMainThread) {
+  const buffer = new SharedArrayBuffer(8);
+  const mutex1 = new Mutex(buffer, 0, true);
+  const mutex2 = new Mutex(buffer, 4, true);
+  console.dir({ mutex1, mutex2 });
+  new Worker(__filename, { workerData: buffer });
+  new Worker(__filename, { workerData: buffer });
+} else {
+  const { threadId, workerData } = threads;
+  const mutex1 = new Mutex(workerData);
+  const mutex2 = new Mutex(workerData, 4);
+
+  const loop = () => {
+    mutex1.enter(() => {
+      console.log(`Entered mutex1 from worker${threadId}`);
+      if (mutex1.leave()) console.log(`Left mutex1 from worker${threadId}`);
+    });
+    mutex2.enter(() => {
+      console.log(`Entered mutex2 from worker${threadId}`);
+      if (mutex2.leave()) console.log(`Left mutex2 from worker${threadId}`);
+    });
+    setTimeout(loop, 0);
+  };
+  loop();
+
+}
