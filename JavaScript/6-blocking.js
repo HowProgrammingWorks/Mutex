cc9e396ce8191497fb1533f2af8d84d88a52f909
tshemsedinov <timur.shemsedinov@gmail.com>
2019-03-22 16:49:08 +0200

    Example to show that Async.wait blocks event-loop

---
+++ JavaScript/6-blocking.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const threads = require('worker_threads');
+const { Worker, isMainThread } = threads;
+
+const LOCKED = 0;
+const UNLOCKED = 1;
+
+class Mutex {
+  constructor(shared, offset = 0, initial = false) {
+    this.lock = new Int32Array(shared, offset, 1);
+    if (initial) Atomics.store(this.lock, 0, UNLOCKED);
+    this.owner = false;
+    if (!Mutex.collection) {
+      Mutex.collection = new Set();
+    }
+    Mutex.collection.add(this);
+  }
+
+  enter() {
+    let prev = Atomics.exchange(this.lock, 0, LOCKED);
+    while (prev !== UNLOCKED) {
+      Atomics.wait(this.lock, 0, LOCKED);
+      prev = Atomics.exchange(this.lock, 0, LOCKED);
+    }
+    this.owner = true;
+  }
+
+  leave() {
+    if (!this.owner) return false;
+    Atomics.store(this.lock, 0, UNLOCKED);
+    Atomics.notify(this.lock, 0, 1);
+    this.owner = false;
+    return true;
+  }
+}
+
+// Usage
+
+if (isMainThread) {
+  const buffer = new SharedArrayBuffer(4);
+  const mutex = new Mutex(buffer, 0, true);
+  console.dir({ mutex });
+  new Worker(__filename, { workerData: buffer });
+  new Worker(__filename, { workerData: buffer });
+} else {
+  const { threadId, workerData } = threads;
+  const mutex = new Mutex(workerData);
+
+  setInterval(() => {
+    console.log(`Interval ${threadId}`);
+  }, 500);
+
+  const loop = () => {
+    mutex.enter();
+    console.log(`Enter ${threadId}`);
+    setTimeout(() => {
+      mutex.leave();
+      console.log(`Leave ${threadId}`);
+      loop();
+    }, 5000);
+  };
+  loop();
+}
